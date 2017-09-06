50c5fdcf2e9570eb271d9bb990780170da94e958
Nathalia Irokawa Fernandes <nanucaif@hotmail.com>
2017-09-05 23:46:11 -0300

    Adicionando projeto calculadora

---
+++ Aula1/Aula1/MainActivity.cs
@@ -0,0 +1,46 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Aula1
+{
+    [Activity(Label = "Aula1", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        //Os componentes no arquivo axml não são visíveis aqui, portanto, é necessário instânciá-los
+
+        //Criando as variáveis para instanciar os objetos do layout (arquivo axml Main),
+        TextView tvMensagem; //não é necessário escrever o mesmo nome do id no arquivo axml
+        Button btnOkay;
+        AutoCompleteTextView tfNome;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            //Explica à qual layout esta página de código irá pertencer
+            SetContentView (Resource.Layout.Main);
+
+            //Instânciando os componentes
+            tvMensagem = (TextView)FindViewById(Resource.Id.tvMensagem);
+            btnOkay = (Button)FindViewById(Resource.Id.btnOkay);
+            tfNome = (AutoCompleteTextView)FindViewById(Resource.Id.tfNome);
+
+            //nome da variável criada = (aquilo que quer converter - componente que vai ser convertido na axml).FindViewbyId(Resource.Id.id do obj)
+            //FindViewById - busca componentes pela Id
+
+            //Criando o método de clique
+            btnOkay.Click += BtnOkay_Click;
+            //Adicionando à ação do botão +- um método chamada btnOkay_Click 
+        }
+
+        private void BtnOkay_Click(object sender, System.EventArgs e)
+        {
+            string nome;
+            nome = tfNome.Text;
+
+            tvMensagem.Text = "Bem vindo ao Xamarin, " + nome + "!";
+        }
+    }
+}
+
---
+++ Calculadora-Aula1/Calculadora-Aula1/MainActivity.cs
@@ -0,0 +1,40 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+
+namespace Calculadora_Aula1
+{
+    [Activity(Label = "Calculadora_Aula1", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        Button btnSomar;
+        TextView tvResultado;
+        EditText nedN1, nedN2;
+        
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+
+            nedN1 = (EditText)FindViewById(Resource.Id.nedN1);
+            nedN2 = (EditText)FindViewById(Resource.Id.nedN2);
+            btnSomar = (Button)FindViewById(Resource.Id.btnSomar);
+            tvResultado = (TextView)FindViewById(Resource.Id.tvResultado);
+
+            btnSomar.Click += BtnSomar_Click;
+        }
+
+        private void BtnSomar_Click(object sender, EventArgs e)
+        {
+            double a, b, res;
+            a = double.Parse(nedN1.Text);
+            b = double.Parse(nedN2.Text);
+            res = a + b;
+            tvResultado.Text = res.ToString();
+        }
+    }
+}
+
